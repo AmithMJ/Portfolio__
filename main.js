cbc4640615a89d572a99646f2ae1e4d7740ef4ab
Amith MJ <amithmj1234@gmail.com.com>
2023-11-24 13:53:48 +0530

    Portfolio

---
+++ main.js
@@ -0,0 +1,103 @@
+/*------NAVIGATION BAR FUNCTION----*/
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+
+
+  /*------ADD SHADOW ON NAVIGATION BAR WHILE SCROLING----*/
+  window.onscroll = function() {headerShadow()};
+    function headerShadow() {
+      const navHeader =document.getElementById("header");
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+      } else {
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+      }
+    }
+
+     /*----------DOWNLOAD CV------------*/
+  
+
+
+     /*----------SCROLL REVEAL ANIMATION------------*/
+
+     const sr = ScrollReveal({
+      origin: "top" ,
+      diatance: "80px" ,
+      duration: 2000 ,
+      reset: true
+     })
+
+     
+     /*----------HOME------------*/
+     sr.reveal('.featured-text-card',{})
+     sr.reveal('.featured-name',{delay: 100})
+     sr.reveal('.featured-text-info',{delay: 200})
+     sr.reveal('.featured-text-btn',{delay: 200})
+     sr.reveal('.social_icons',{delay: 200})
+     sr.reveal('.featured-image',{delay: 300})
+     
+
+      
+     /*----------HOME------------*/
+     sr.reveal('.project-box',{interval: 200})
+
+     
+     /*----------HEADINGS------------*/
+     sr.reveal('.top-header',{})
+     
+     /*----------ABOUT INFO & CONTACT INFO------------*/
+     const srLeft = ScrollReveal({
+      origin: 'left',
+      distance: '80px',
+      duration: 2000,
+      reset: true
+    })
+    
+    srLeft.reveal('.about-info',{delay: 100})
+    srLeft.reveal('.contact-info',{delay: 100})
+
+     
+     /*----------ABOUT SKILLS & FORM------------*/
+     const srRight = ScrollReveal({
+      origin: 'right',
+      distance: '80px',
+      duration: 2000,
+      reset: true
+    })
+    
+    srRight.reveal('.skills-box',{delay: 100})
+    srRight.reveal('.form-control',{delay: 100})
+
+     
+     /*-------------CHANGE ACTIVE LINK---------------*/
+
+     const sections = document.querySelectorAll('section[id]')
+
+     function scrollActive() {
+      const scrollY = window.scrollY;
+
+      sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+          sectionId = current.getAttribute('id')
+
+          if(scrollY > sectionTop && scrollY<= sectionTop + sectionHeight)
+          
+          document.querySelector('.nav-menu a[href*='+ sectionId  + ']').classList.add('active-link')
+
+          else{
+            document.querySelector('.nav-menu a[href*='+ sectionId + ']').classList.remove('active-link')
+          }
+      })
+     }
+     window.addEventListener('scroll',scrollActive)
